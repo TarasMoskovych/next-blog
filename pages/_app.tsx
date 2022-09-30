afefcd5b43636cdf078989d0a998eb3dbf20fefe
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2022-09-30 17:33:48 +0300

    add basic layout styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'styles/index.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+export default ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
