faa922070eef90a7a1458b0b2cfca1d08410d1b5
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2022-11-28 17:30:14 +0200

    fix fonts

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -13,11 +13,15 @@ const PageLayout = ({ children, className, title = 'Next Blog' }: Props) => {
     <>
       <Head>
         <title>{title}</title>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
+        <link href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&display=swap' rel='stylesheet' />
+        <link href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0&display=swap' rel='stylesheet' />
       </Head>
       <Header />
-        <main className={`nb-page-wrapper ${className || ''}`}>
-          {children}
-        </main>
+      <main className={`nb-page-wrapper ${className || ''}`}>
+        {children}
+      </main>
       <Footer />
     </>
   );
